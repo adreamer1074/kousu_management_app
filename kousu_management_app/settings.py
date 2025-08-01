d3754764e1506615d8201f39588581db2816542c
MaiVanQuang <maivan.quang@mash.ne.jp>
2025-08-01 17:29:56 +0900

    カッコイイダッシュボード追加

--- kousu_management_app/settings.py
+++ kousu_management_app/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize', 
+    # ローカルアプリケーション
     'apps.core',
     'apps.users',
     'apps.projects',
